00287a02c6fddbb07e47cb6de02d5503f1fcf966
RamonWill <ramonwilliams@hotmail.co.uk>
2020-04-21 19:19:43 +0100

    configurations

---
+++ Models/config.py
@@ -0,0 +1,11 @@
+class Configurations(object):
+    # Database
+    DATABASE_NAME = ""  # Default value "source.db"
+
+    # Oanda Account
+    OANDA_ACCOUNT = ""
+
+    # API_Keys
+    OANDA_KEY = ""
+    AV_KEY = ""
+    NEWS_KEY = ""
