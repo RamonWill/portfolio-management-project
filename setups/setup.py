71a3fecdb187f24976bb9125bb1f140a5da7e632
RamonWill <ramonwilliams@hotmail.co.uk>
2021-03-16 17:49:46 +0000

    remove set up

--- setups/setup.py
+++ setups/setup.py
@@ -23,25 +23,3 @@ def Setup_database_tables(path):
     c.close()
     conn.close()
     print(f"A new database has been created in {path}")
-
-
-def check_configurations():
-    "Checks if the APIs have been set up."
-    c = Configurations()
-    config_keys = (c.OANDA_KEY,
-                   c.AV_KEY,
-                   c.NEWS_KEY)
-
-    config_account = c.OANDA_ACCOUNT
-    config_database = c.DATABASE_NAME
-    if "" in config_keys:
-        number = config_keys.count("")
-        raise ValueError(f"{number} API keys are missing. "
-                         "Please add these to the config file")
-    if config_account == "":
-        raise ValueError("The Oanda account is missing from the API. "
-                         "Please add it to the config file.")
-    if config_database == "":
-        raise ValueError("The application is not connected to a database. "
-                         "Please set a database name to the config file.")
-    print(f"Connected to {config_database}")
